ac8a1c8d522d8df424e3ef56c8122cc7bebbbe1e
visalrotanak <visalrotanak7@gmail.com>
2023-11-22 12:55:19 +0700

    clean project

--- api/models/category.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from .basemodel import BaseModel
-
-
-class Category(BaseModel):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
