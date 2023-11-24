5d83320a8bdb58ca7e9e3f92578903f8ccfd40a3
visalrotanak <visalrotanak7@gmail.com>
2023-11-24 10:40:25 +0700

    Update beta0.0.3

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,3 +2,4 @@ from .product import *
 from .category import *
 from .tag import *
 from .customer import *
+from .coupon import *
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -2,3 +2,4 @@ from .category_view import *
 from .product_view import *
 from .user_view import *
 from .customer_view import *
+from .coupon_view import *
